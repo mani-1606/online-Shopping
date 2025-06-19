3e441fdf7f714198b85d47ffc553d548e31aa9c6
mani <mpanchadi@gmail.com>
2025-06-19 17:27:49 +0530

    Update CategoryController and application.properties

--- mani/src/main/java/com/javaboy/mani/controller/CategoryController.java
+++ mani/src/main/java/com/javaboy/mani/controller/CategoryController.java
@@ -1,4 +1,77 @@
 package com.javaboy.mani.controller;
 
+import com.javaboy.mani.model.Category;
+import com.javaboy.mani.response.ApiResponse;
+import com.javaboy.mani.service.category.ICategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/categories")
 public class CategoryController {
+    private final ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories(){
+        try {
+            List<Category> categories =  categoryService.getAllCategories();
+            return ResponseEntity.ok(new ApiResponse("Categories retrieved successfully", categories));
+        } catch (Exception e){
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("error : ", e.getMessage()));
+        }
+    }
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category category) {
+        try {
+            Category savedCategory = categoryService.addCategory(category);
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ApiResponse("Category added successfully", savedCategory));
+        } catch (Exception e) {
+            return ResponseEntity.status(CONFLICT).body(new ApiResponse("error : ", e.getMessage()));
+        }
+    }
+    @GetMapping("/category/{id}/category")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id){
+        try {
+            Category category = categoryService.findCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Category retrieved successfully", category));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("error : ", e.getMessage()));
+        }
+    }
+    @PutMapping("/category/{id}/update")
+    public ResponseEntity<ApiResponse> updateCategory(@RequestBody Category category, @PathVariable Long id){
+        try {
+            Category updatedCategory = categoryService.updateCategory(category, id);
+            return ResponseEntity.ok(new ApiResponse("Category updated successfully", updatedCategory));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("error : ", e.getMessage()));
+        }
+    }
+    @DeleteMapping("/category/{id}/delete")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id){
+        try {
+            categoryService.deleteCategory(id);
+            return ResponseEntity.ok(new ApiResponse("Category deleted successfully", null));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("error : ", e.getMessage()));
+        }
+    }
+    @GetMapping("/category/{name}/category")
+    public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String name){
+        try {
+            Category category = categoryService.findCategoryByName(name);
+            return ResponseEntity.ok(new ApiResponse("Category retrieved successfully", category));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("error : ", e.getMessage()));
+        }
+    }
+
 }
